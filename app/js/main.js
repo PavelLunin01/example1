94474b18bac54f35c2160cdb783a74c161b2f0bd
PavelLunin01 <luninpavel01@gmail.com>
2022-04-12 19:17:30 +0300

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,11 @@ $(function () {
     dots: true,
     arrows: false,
   });
+
+  $('.star').rateYo({
+    ratedFill: "#ffc35b",
+    normalFill: "#8d8d8d",
+    starWidth: '16px',
+    readOnly: true,
+  });
 });
